32cb4258465307cb639a2283771c1ae4ebab233a
Sarah D. Perea <sdperea.1@gmail.com>
2019-06-26 20:07:31 +0800

    'test' command working

--- index.js
+++ index.js
@@ -2,16 +2,34 @@ const Discord = require('discord.js');
 // ytdl provides an API to get a readable stream for a YouTube video
 const ytdl = require('ytdl-core');
 const prefix = '$';
+const fs = require('fs');
 let voiceChannelConnection = false;
 
 // Create client and login
 const client =  new  Discord.Client ();
 
+client.commands = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+  if (err) console.log(err)
+  let jsfile = files.filter(f => f.split(".").pop() === "js");
+  if (jsfile.length <= 0) {
+    console.log("No command found.");
+    return;
+  }
+
+  jsfile.forEach((f, i) => {
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded and working!`);
+    client.commands.set(props.help.name, props);
+  })
+})
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', message => {
+client.on('message', async message => {
 
   if (!message.guild) return;
 
@@ -20,6 +38,9 @@ client.on('message', message => {
     let cmd = args[0];
        
     args = args.splice(1); // arguments array after the command
+    let cmdFile = client.commands.get(cmd);
+    if (cmdFile) cmdFile.run(client, message, args);
+    else console.log('Sorry, no command found.')
     switch(cmd) {
     	case 'join':
 	    if (message.member.voiceChannel) {
@@ -52,4 +73,4 @@ client.on('message', message => {
 
 });
 
-client.login(process.env.DISCORD_BOT_TOKEN);
+client.login(process.env.DISCORD_BOT_SAMGYUPSAR_TOKEN);
