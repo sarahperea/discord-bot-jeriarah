d95c6ea3319cab91713f21364490d90de911adf4
Sarah D. Perea <sdperea1@up.edu.ph>
2019-06-27 18:11:59 +0800

    Revert "Dev"

--- commands/test.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js');
-
-module.exports.run = async(client, message, args) => {
-	message.channel.send("test success");
-}
-
-module.exports.help = {
-	name: 'test'
-}
