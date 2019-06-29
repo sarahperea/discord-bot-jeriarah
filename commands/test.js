ad2e1b2d6f665052e3b6705ff7e2126167ac0400
Sarah D. Perea <sdperea.1@gmail.com>
2019-06-29 11:13:23 +0800

    Support for multiple args

--- commands/test.js
+++ commands/test.js
@@ -3,19 +3,20 @@ const axios = require('axios');
 const apiKey = process.env.MEETUP_API_KEY; 
 
 module.exports.run = async(client, message, args) => {
-	const topic = args[0];
-	let names = [];
-	axios.get(`https://api.meetup.com/2/open_events?topic=${topic}&time=,1w&key=${apiKey}`, {}).then(res => {
-		for(let r of res.data.results) {
-			if(r.venue) {
-				names = [...names, r.venue.name];
+	args.map((topic) => {
+		let names = [];
+		axios.get(`https://api.meetup.com/2/open_events?topic=${topic}&time=,1w&key=${apiKey}`, {}).then(res => {
+			for(let r of res.data.results) {
+				if(r.venue) {
+					names = [...names, r.venue.name];
+				}
 			}
-		}
-		let events = names.join().substring(0, 2000);
-		message.channel.send(events);
-	}).catch(err => {
-		console.log(err);
-	});
+			let events = names.join().substring(0, 2000);
+			message.channel.send(events);
+		}).catch(err => {
+			console.log(err);
+		});		
+	})
 }
 
 module.exports.help = {
