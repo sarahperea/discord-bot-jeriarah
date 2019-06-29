77e21a660a6a9c6bcb72e1000fcecb280a6e30f2
Jerico Rillo <jericorillo@gmail.com>
2019-06-29 11:09:00 +0800

    test axios

--- commands/test.js
+++ commands/test.js
@@ -1,7 +1,21 @@
 const Discord = require('discord.js');
+const axios = require('axios');
+const apiKey = process.env.MEETUP_API_KEY; 
 
 module.exports.run = async(client, message, args) => {
-	message.channel.send("test success");
+	const topic = args[0];
+	let names = [];
+	axios.get(`https://api.meetup.com/2/open_events?topic=${topic}&time=,1w&key=${apiKey}`, {}).then(res => {
+		for(let r of res.data.results) {
+			if(r.venue) {
+				names = [...names, r.venue.name];
+			}
+		}
+		let events = names.join().substring(0, 2000);
+		message.channel.send(events);
+	}).catch(err => {
+		console.log(err);
+	});
 }
 
 module.exports.help = {
